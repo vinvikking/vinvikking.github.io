8c5589d338e42d32572842bf50ade363337d9bf1
Vincent <vincentroeland1997@outlook.com>
2018-10-11 22:25:12 +0200

    Commit les 9

--- JS/les10.js
+++ JS/les10.js
@@ -4,20 +4,11 @@ window.addEventListener('load', init);
 function init(){
      const submitButton = document.getElementById('submit-btn');
      var i = 0;
-     submitButton.addEventListener('click', ClickeventHandles)
+     
      submitButton.addEventListener('click', function(){run(i++)});
 }
 
 
-
-
-function ClickeventHandles(event){
-    event.preventDefault();
-
-    
-   // run();
-    
-}
 
 function run(i){
 
@@ -26,19 +17,59 @@ function run(i){
     const list = document.createElement("p");
     list.innerText = i;
 
-    if(i > 10){
-        runstyle();
-        console.log(i);
+    if(i > 10 | i > 20){
+        document.getElementById('submit-btn').style.width = "30px";
+        document.getElementById('submit-btn').style.height = "10px";
+    } else if (i > 20){
+        document.getElementById('submit-btn').style.width = "10px";
+        document.getElementById('submit-btn').style.height = "5px";
     } 
 
     j.appendChild(list);
     
 }
 
-function runstyle(){
-    document.getElementById('submit-btn').style.width = "5070px";
+
+// Opdracht 2
+
+const button = document.getElementById("add");
+button.addEventListener("click", add)
+
+
+
+function add(event){
+    event.preventDefault();
+
+    const inputText = document.getElementById('name').value;
+
+    const text = document.getElementById('producten');
+    const li = document.createElement('td');
+
+    li.innerText = inputText;
+    
+    const inputPrice = document.getElementById('price').value;
+    console.log(inputPrice)
+    const tekst = document.getElementById('prijs');
+    const p = document.createElement('td');
+
+    p.innerText = inputPrice;
+
+    if(inputPrice !== '' && inputText !== ''){
+        
+        text.appendChild(li);
+        tekst.appendChild(p);
+        
+    }
+    else{
+        alert("vul de velden in");
+    }
+
 }
 
+
+
+
+ 
 
 // addeventlistener
 
--- JS/les9.js
+++ JS/les9.js
@@ -6,17 +6,46 @@ const listItems = document.getElementsByTagName('td').values
 
 console.log(listItems);
 
-
-
-const lastItemFromListItems = listItems[listItems.length-1].innerText;
-
-console.log(lastItemFromListItems);
-const lastitem1 = listItems[3].innerText;
-console.log(lastitem1);
-
-const lastItemFromListItems1 = document.getElementById('cijfers').lastChild.innerText;
-
-
+// Opdracht 1:
+function opdracht1() {
+    const list = document.getElementsByTagName('tr');
+    
+    let gradesArray = [];
+    let gradesArraySum = 0;
+    
+    for (let i = 0; i < list.length; i++) {
+        let listItem = list[i];
+        let getGrade = listItem.lastElementChild.innerText;
+        let parsedValue = parseFloat(getGrade);
+        let generateArray = gradesArray.push(parsedValue);
+        let averageGrade = gradesArraySum += gradesArray[i];
+    };
+    
+    let overallGrade = gradesArraySum / list.length;
+    const table = document.getElementById('table');
+    
+    let row = table.insertRow(list.length);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    
+    cell1.innerText = "Gemiddelde cijfer";
+    cell2.innerText = overallGrade.toFixed(1);
+    }
+    opdracht1();
+    
+    // Ordracht 2:
+    function colorChange() {
+        let lists = document.getElementsByTagName('li');
+        
+        for (let list in lists) {
+            if (list % 2 == 0) {
+                lists[list].classList.add('colorChange');
+            }
+        }
+    }
+    
+    colorChange();
+    
 
 
 
